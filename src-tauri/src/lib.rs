3a1b40368d12fbaa2a1a58e9797de7cbd5e44feb
MEDINA <cemp493gmail.com>
2026-06-17 19:25:37 -0600

    feat: conexión SQLite exitosa y refactorización de CSS

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,4 +1,5 @@
-// Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
+use tauri_plugin_sql::{Builder as SqlBuilder, Migration, MigrationKind};
+
 #[tauri::command]
 fn greet(name: &str) -> String {
     format!("Hello, {}! You've been greeted from Rust!", name)
@@ -6,9 +7,36 @@ fn greet(name: &str) -> String {
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
+    // Definimos nuestras Migraciones (El esquema de la base de datos)
+    let migraciones = vec![Migration {
+        version: 1,
+        description: "crear_tablas_catalogo",
+        sql: "
+                CREATE TABLE IF NOT EXISTS categorias (
+                    id INTEGER PRIMARY KEY AUTOINCREMENT,
+                    nombre TEXT NOT NULL
+                );
+
+                CREATE TABLE IF NOT EXISTS productos (
+                    id INTEGER PRIMARY KEY AUTOINCREMENT,
+                    categoria_id INTEGER,
+                    nombre TEXT NOT NULL,
+                    precio_base REAL NOT NULL,
+                    FOREIGN KEY (categoria_id) REFERENCES categorias (id)
+                );
+            ",
+        kind: MigrationKind::Up,
+    }];
+
+    // Ensamblamos la aplicación y activamos el plugin SQL
     tauri::Builder::default()
+        .plugin(
+            SqlBuilder::default()
+                .add_migrations("sqlite:pizzeria.db", migraciones)
+                .build(),
+        )
         .plugin(tauri_plugin_opener::init())
         .invoke_handler(tauri::generate_handler![greet])
         .run(tauri::generate_context!())
-        .expect("error while running tauri application");
+        .expect("error al ejecutar la aplicación tauri");
 }

3bdbcad68770cf57869ba8ec26af484924c593d2
MEDINA <cemp493gmail.com>
2026-06-17 19:01:26 -0600

    feat: inicialización del proyecto y limpieza del lienzo base

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+
+function App() {
+  return (
+    <main className="container">
+      <h1>Pizzería POS</h1>
+      <p>Sistema en construcción...</p>
+    </main>
+  );
+}
+
+export default App;
